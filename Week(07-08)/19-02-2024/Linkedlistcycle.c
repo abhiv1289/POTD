d0bb2cc9763fd47e486e0503556561430c7a11a7
abhiv1289 <abhiv9255@gmail.com>
2024-02-19 18:17:43 +0530

    new

---
+++ Week(07-08)/19-02-2024/Linkedlistcycle.c
@@ -0,0 +1,39 @@
+//Linked list cycle
+//link--> leetcode.com/problems/palindrome-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool isPalindrome(struct ListNode* head) {
+    if(head==NULL)
+    {
+        return true;
+    }
+   int length=0;
+   int arr[100000];
+   
+  struct ListNode *temp=head;
+  while(temp!=NULL)
+  {
+      arr[length]=temp->val;
+      length++;
+      temp=(temp)->next;
+  }
+  int s=0;
+  int e=length-1;
+  while(s<=e)
+  {
+      if(arr[s]!=arr[e])
+      {
+          return false;
+      }
+      s++;
+      e--;
+
+  }
+  return true;
+}
